46b7d6f9167b19cbf7ae16ab9c8c18064cb3c248
huang-hua1212 <hahahazzz123@gmail.com>
2022-05-22 20:11:43 +0800

    commit on first

---
+++ controller/processExcel_Buffer.js
@@ -0,0 +1,15 @@
+const xl = require('xlsx');
+
+async function readExceLFile(encodeFile64) {
+    const workBook = xl.read(encodeFile64);
+    const sheetNames = workBook.SheetNames;
+    const excelObj = {}
+    await sheetNames.forEach((sheetName)=>{
+        const workSheet = workBook.Sheets[sheetName];
+        const sheetContent = xl.utils.sheet_to_json(workSheet);
+        excelObj[sheetName]= sheetContent;
+    })
+    return excelObj;
+}
+
+module.exports = readExceLFile;
---
+++ controller/processExcel_Local.js
@@ -0,0 +1,13 @@
+
+const xl =require('xlsx');
+//workbook 物件，指的是整份 Excel 文件。我們在使用 js-xlsx 讀取 Excel 文件之後就會獲得 workbook 物件。
+const workBook =  xl.readFile("./files/Book1.xlsx");
+const sheetNames = workBook.SheetNames; // 返回 ['sheet1', 'sheet2']
+const workSheet = workBook.Sheets[sheetNames[0]];
+
+
+//返回json資料
+// const data =xl.utils.sheet_to_json(worksheet);
+const data =xl.utils.sheet_to_json(workSheet);
+
+console.log(data);
